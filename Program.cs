d3cee457bd300bc5e6c80f9b97a9138b45635c51
netonblpb <skullbog@gmail.com>
2022-02-18 15:43:56 +0300

    Task34 complete

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Написать программу для замены элементов массива на противоположные.
+
+Console.Clear();
+
+Random rnd = new Random();
+
+Console.Write("На сколько элементов задаём массив? ");
+int elem = int.Parse(Console.ReadLine());
+int[] array = new int[elem];
+
+for (int i = 0; i < elem; i++)
+{
+    array[i] = rnd.Next(0, 100);
+    Console.Write(array[i] + "\t");
+}
+
+Console.WriteLine();
+int change = 0;
+for (int j = 0; j < elem/2; j++)
+{
+    change = array[j];
+    array[j] = array[elem - j -1];
+    array[elem - j - 1] = change;    
+}
+
+for(int u = 0; u < elem; u++)
+{
+Console.Write(array[u] + "\t");
+}
